55f6eac64e5f448f445e62210455141b8353d597
Adi Lichy <adi_lichy@yahoo.com>
2019-05-05 00:07:47 +0300

    updated

--- calculate.cpp
+++ calculate.cpp
@@ -8,8 +8,27 @@ std::string calculateBullAndPgia(std::string choose, std::string guess) {
 	if (choose.length() != guess.length())
 		throw "inequal length";
 	int i = 0,j = 0;
-	//Check for bulls
-	//Check for pgia
+	for (size_t i = 0; i < choose.length(); i++)
+	{
+		if (guess[i] == choose[i]) {
+			bull++;
+			guess[i] = '*';
+			choose[i] = '*';
+		}
+	}
+	for (size_t i = 0; i < choose.length(); i++)
+	{
+		if (choose[i] != '*') {
+			for (size_t j = 0; j < guess.length(); j++)
+			{
+				if (choose[i] == guess[j]) {
+					pgia++;
+					guess[j] = '*';
+					break;
+				}
+			}
+		}
+	}
 	reply = std::to_string(bull).append(",").append(std::to_string(pgia));
 	return reply;
 };
