871f39a91ec72e385fbdbb53fc1bd85e3a91ad7d
Adi Lichy <adi_lichy@yahoo.com>
2019-05-07 12:32:46 +0300

    hotfix
    -remove unused code parts
    -added a few comments to easy the under standing of algorithm

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -1,9 +1,6 @@
 #include "SmartGuesser.hpp"
 
 
-
-
-
 std::string bullpgia::SmartGuesser::fillCode(char digit)
 {
 	std::string res = "";
@@ -66,43 +63,51 @@ void bullpgia::SmartGuesser::learn(std::string reply)
 	if (reply == Respones(length, 0))
 		return;
 	std::istringstream(reply.substr(0, reply.find(","))) >> bull;
-	std::istringstream(reply.substr(1 + reply.find(","), reply.length() - 1)) >> pgia;
 	if ((turn-1) <= 10) {
 		if (bull > 0) {
 			bull_count[myguess[0]-'0'] = bull;
 		}
 	}
 	else {
+		//if did not reach end of guess or did not finsh find all the current digit bulls
 		if (track < length && !pick) {
+			//didn't have an old bull;
 			if (oldbull > -1) {
+				//if oldbull is bigger meaning we lost a bull so current digit was a bull
 				if (oldbull > bull)
 				{
-					finalguess[track] = currentdigit;
+					finalguess[track] = currentdigit;//save digit in final guess in the correct postion
 					bull_count[currentdigit]--;
 				}
+				// if oldbull is smaller meaning we have gained a bull from the replace digit
 				else if (oldbull < bull)
 				{
-					finalguess[track] = swap_digit;
+					finalguess[track] = swap_digit;//save digit in final guess in the correct postion
 					bull_count[swap_digit]--;
 				}
+				//check if still more bulls to find in current digit
 				if (bull_count[currentdigit] > 0)
 				{
 					track++;
 					myguess[track] = '0' + swap_digit;
 				}
+				//else no more bulls left in current digit and should pick an new digit to check
 				else pick = !pick;
 			}
 			else {
+				//without checking the current guess swap digit should occur only once
 				myguess[track] = '0' + swap_digit;
 			}
-			oldbull = bull;
+			oldbull = bull;//update oldbull
 		}
+		//pick new number to know its bull
 		if (pick){
 			grpcheck = GroupsLeft(bull_count);
-			if (grpcheck <= 1) {
+			if (grpcheck <= 1) {//if less then 1 setup final guess
 				myguess = setFinalGuess(finalguess, lastdigit);
 			}
 			else {
+				//pick number to fill on.
 				for (size_t i = 0; i < 10; i++)
 				{
 					if (bull_count[i] > 0)
@@ -115,6 +120,7 @@ void bullpgia::SmartGuesser::learn(std::string reply)
 						break;
 					}
 				}
+				//pick number which replace the old one.
 				for (size_t j = 0; j < 10; j++)
 				{
 					if (bull_count[j] > 0 && j != currentdigit)
@@ -127,6 +133,7 @@ void bullpgia::SmartGuesser::learn(std::string reply)
 
 std::string bullpgia::SmartGuesser::guess()
 {
+	//first fill bulls
 	switch (turn)
 	{
 	case 1:
@@ -175,7 +182,6 @@ void bullpgia::SmartGuesser::startNewGame(uint length)
 	currentdigit = 0;
 	lastdigit = 0;
 	pick = true;
-	
 	for (size_t i = 0; i < 10; i++)
 	{
 		bullpgia::SmartGuesser::bull_count[i] = 0;
@@ -187,192 +193,4 @@ void bullpgia::SmartGuesser::startNewGame(uint length)
 	}
 }
 
-/*
-std::vector<int> bullpgia::SmartGuesser::getNextGuess(std::vector<std::vector<int>> nextGuesses)
-{
-	std::vector<int> nextGuess;
-
-	for (int i = 0; i < nextGuesses.size(); ++i) {
-		if (std::find(bullpgia::SmartGuesser::candidateSolutions.begin(), bullpgia::SmartGuesser::candidateSolutions.end(), nextGuesses[i]) != bullpgia::SmartGuesser::candidateSolutions.end()) {
-			return nextGuesses[i];
-		}
-	}
-	for (int j = 0; j < nextGuesses.size(); ++j) {
-		if (std::find(bullpgia::SmartGuesser::combinations.begin(), bullpgia::SmartGuesser::combinations.end(), nextGuesses[j]) != bullpgia::SmartGuesser::combinations.end()) {
-			return nextGuesses[j];
-		}
-	}
-
-	return nextGuess;
-}
-
-std::vector<int> bullpgia::SmartGuesser::getRandomCode()
-{
-	std::vector<int> code;
-	int max = 9;
-	int min = 0;
-	int random;
-
-	for (int i = 0; i < length; ++i) {
-
-		random = min + (rand() % (max - min + 1));
-		code.push_back(random);
-	}
-
-	return code;
-}
-
-void bullpgia::SmartGuesser::createSet()
-{
-	std::vector<int> current(length, 0);
-	std::vector<int> elements;
-
-	for (int i = 0; i <= 9; ++i) {
-		elements.push_back(i);
-	}
-
-	combinationRecursive(length, 0, current, elements);
-}
-
-void bullpgia::SmartGuesser::combinationRecursive(int combinationLength, int position, std::vector<int>& current, std::vector<int>& elements)
-{
-	if (position >= combinationLength) {
-		combinations.push_back(current);
-		return;
-	}
-
-	for (int j = 0; j < elements.size(); ++j) {
-		current[position] = elements[j];
-		combinationRecursive(combinationLength, position + 1, current, elements);
-	}
-	return;
-}
-
-void bullpgia::SmartGuesser::removeCode(std::vector<std::vector<int>>& set, std::vector<int> currentCode)
-{
-	int index;
-	for (auto it = set.begin(); it != set.end(); it++) {
-		index = distance(set.begin(), it);
-
-		if (set[index] == currentCode) {
-			set.erase(set.begin() + index);
-			break;
-		}
-	}
-}
-
-void bullpgia::SmartGuesser::pruneCodes(std::vector<std::vector<int>>& set, std::vector<int> currentCode, std::string currentResponse)
-{
-	int index;
-	std::vector<std::vector<int>>::iterator it = set.begin();
-
-	while (it != set.end()) {
-		index = distance(set.begin(), it);
-
-		if (currentResponse != calculateBullAndPgia(vector_to_string(currentCode), vector_to_string(set[index]))) {
-			it = set.erase(set.begin() + index);
-		}
-		else {
-			it++;
-		}
-	}
-}
-
-std::vector<std::vector<int>> bullpgia::SmartGuesser::minmax()
-{
-	std::map<std::string, int> scoreCount;
-	std::map<std::vector<int>, int> score;
-	std::vector<std::vector<int>> nextGuesses;
-	int max, min;
-
-	for (int i = 0; i < combinations.size(); ++i) {
-
-		for (int j = 0; j < candidateSolutions.size(); ++j) {
-
-			std::string pegScore = calculateBullAndPgia(vector_to_string(combinations[i]), vector_to_string(candidateSolutions[j]));
-			if (scoreCount.count(pegScore) > 0) {
-				scoreCount.at(pegScore)++;
-			}
-			else {
-				scoreCount.emplace(pegScore, 1);
-			}
-		}
-
-		max = getMaxScore(scoreCount);
-		score.emplace(combinations[i], max);
-		scoreCount.clear();
-	}
-
-	min = getMinScore(score);
-
-	for (auto elem : score) {
-		if (elem.second == min) {
-			nextGuesses.push_back(elem.first);
-		}
-	}
-
-	return nextGuesses;
-}
-
-int bullpgia::SmartGuesser::getMaxScore(std::map<std::string, int> inputMap)
-{
-	int max = 0;
-	for (auto elem : inputMap) {
-		if (elem.second > max) {
-			max = elem.second;
-		}
-	}
-
-	return max;
-}
-
-int bullpgia::SmartGuesser::getMinScore(std::map<std::vector<int>, int> inputMap)
-{
-	int min = std::numeric_limits<int>::max();
-	for (auto elem : inputMap) {
-		if (elem.second < min) {
-			min = elem.second;
-		}
-	}
-
-	return min;
-}
-
-std::string bullpgia::SmartGuesser::vector_to_string(std::vector<int> vec)
-{
-	std::string result = "";
-	for (size_t i = 0; i < vec.size(); i++)
-	{
-		result += vec[i];
-	}
-	return result;
-}
-
-void bullpgia::SmartGuesser::learn(std::string reply)
-{
-	if (first_try) {
-		createSet();
-		bullpgia::SmartGuesser::candidateSolutions.insert(bullpgia::SmartGuesser::candidateSolutions.end(), bullpgia::SmartGuesser::combinations.begin(), bullpgia::SmartGuesser::combinations.end());
-		first_try = false;
-	}
-	removeCode(bullpgia::SmartGuesser::combinations, bullpgia::SmartGuesser::currentGuess);
-	removeCode(bullpgia::SmartGuesser::candidateSolutions, currentGuess);
-
-	//Remove from candidateSolutions,
-	//any code that would not give the same response if it were the code
-	pruneCodes(bullpgia::SmartGuesser::candidateSolutions, bullpgia::SmartGuesser::currentGuess, reply);
-
-	//Calculate Minmax scores
-	bullpgia::SmartGuesser::nextGuesses = minmax();
-
-	//Select next guess
-	bullpgia::SmartGuesser::currentGuess = getNextGuess(bullpgia::SmartGuesser::nextGuesses);
-}
-std::string bullpgia::SmartGuesser::guess()
-{
-	if (first_try)
-		bullpgia::SmartGuesser::currentGuess = getRandomCode();
-	return vector_to_string(bullpgia::SmartGuesser::currentGuess);
-}
-*/
 
--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -9,31 +9,12 @@ namespace bullpgia {
 	class SmartGuesser : public Guesser
 	{
 	private:
-		/*
-		bool first_try = true;
-		static std::vector<std::vector<int>> combinations; //Master set of combinations 1111 to 6666
-		static std::vector<std::vector<int>> candidateSolutions;
-		static std::vector<std::vector<int>> nextGuesses;
-		static std::vector<int> currentGuess;
-		std::string myguess;
-		std::vector<int> getNextGuess(std::vector<std::vector<int>> nextGuesses);
-		std::vector<int> getRandomCode();
-		void createSet();
-		void combinationRecursive(int combinationLength, int position, std::vector<int> &current, std::vector<int> &elements);
-		void removeCode(std::vector<std::vector<int>> &set, std::vector<int> currentCode);
-		void pruneCodes(std::vector<std::vector<int>> &set, std::vector<int> currentCode, std::string currentResponse);
-		std::vector<std::vector<int>> minmax();
-		int getMaxScore(std::map<std::string, int> inputMap);
-		int getMinScore(std::map<std::vector<int>, int> inputMap);
-		std::string vector_to_string(std::vector<int> vec);
-	*/
 		std::string myguess;
 		std::vector<char> blacklist;
 		int bull_count[10];
 		std::vector<int> finalguess;
-		int bull, pgia,currentdigit,swap_digit,grpcheck,lastdigit;
-		int oldpgia=0,oldbull = -1,track;
-		int turn;
+		int bull, pgia,currentdigit,swap_digit,grpcheck,lastdigit,track,turn;
+		int oldpgia=0,oldbull = -1;
 		bool pick;
 		uint length;
 		std::string fillCode(char digit);
